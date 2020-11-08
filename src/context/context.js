f7b8de0e7d52187d14a04687691749b0f91d2461
yarinTag <taguori5@gmai.com>
2020-11-08 22:05:27 +0200

    Update mockData and start working on Info.js

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,23 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+//Invoke the method, and now we have the context.
+
+const GithubContext=React.createContext();
+
+//We have access to the provider as well as the consumer.
+
+//we'll render all our application
+const GithubProvider=({children})=>{
+    const [GithubUser,setGithubUser]=useState(mockUser);
+    const [repos,setRepos]=useState(mockRepos);
+    const [followers,setFollowers]=useState(mockFollowers);
+    return(
+        <GithubContext.Provider value={{GithubUser,repos,followers}}>
+            {children}
+        </GithubContext.Provider>
+    );
+}
+
+export{GithubProvider,GithubContext};
